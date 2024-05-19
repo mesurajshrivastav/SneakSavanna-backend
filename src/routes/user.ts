36dc72afe0fd11c15754e1baeec985b7e35159e6
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-05-19 12:24:23 +0530

    give auth to admin only

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { adminOnly } from "../middleware/auth.js"
 
 const app = express.Router();
 
@@ -7,11 +8,12 @@ const app = express.Router();
 // route - /api/v1/user/new
 app.post("/new",newUser);
 
+
 // route - /api/v1/user/all
-app.get("/all",getAllUsers);
+app.get("/all",adminOnly ,getAllUsers);
 
 
 // route - /api/v1/user/Dynamic id
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly,deleteUser);
 
 export default app;
